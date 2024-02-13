3c23643929f3c2e1a7129852b12604c37035c3f5
SlavekRylich <xrylic00@vutbr.cz>
2024-02-13 20:43:50 +0100

    change name server.py

--- IEC104_SERVER/server.py
+++ IEC104_SERVER/server.py
@@ -1,145 +1,203 @@
-import acpi
-import asdu
+# Import required modules
 import socket
-import struct
-import logging
-import binascii
+import threading
+import time
+import acpi
 from config_loader import ConfigLoader
 from CommModul import CommModule
-from multiprocessing import Process
-from bitstring import BitStream
-import Frame
 
-LOG = logging.getLogger()
-logging.basicConfig(level=logging.DEBUG)
+HOST = '127.0.0.1'
+PORT = 1234 # You can use any port between 0 to 65535
+LISTENER_LIMIT = 5
 
-
-class IEC104Server(object):
-
-    def __init__(self, ip, port):
+class ServerIEC104():
+    
+    # constructor for IEC104 server
+    def __init__(self, ip, port): 
         self.ip = ip
         self.port = port
         self.active_session = False
         self.connected = False
         self.type_frame = ""
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((self.ip, self.port))
+        
+        self.active_clients = [] # List of all currently connected users
+    
+    # Function to listen for upcoming messages from a client
+    def listen_for_messages(self,client, username):
 
-    def start(self):
-        self.server_socket.listen(2)
-        print(f"Server listening on {self.ip}:{self.port}")
-        
-        while True:
-            self.client_socket, client_address = self.server_socket.accept()            
-            self.client_handler = Process(target=self.handle_client, args=(self.client_socket, client_address,))
-            self.client_handler.start()
+        while 1:
 
-    def handle_client(self, client_socket, client_address):
-        
-        comm_module = CommModule(client_socket)
-        frame = Frame.Frame()
-        try:
-            self.client_socket.settimeout(acpi.T0)
-            print(f"[NEW CONNECTION] {client_address} connected.")
-            self.connected = True
-            packet = comm_module.receive_data()
-            
-            header = packet[0]
-            if (header[2] & 3) == 3: 
-                print(f"U format {header[2] & 0xFF}")
-                self.type_frame = "U"
-                header = header[2]
-                print(header)
-                if self.type_frame == "U":
-                    if header == acpi.STARTDT_ACT:
-                        data = frame.pack(acpi.STARTDT_CON)
-                        comm_module.send_data(data)
-                        print("prijato startdt act, posilam startdt con")
-                        self.active_session = True
-                    elif header == acpi.STOPDT_ACT:
-                        data = frame.pack(acpi.STOPDT_CON)
-                        comm_module.send_data(data)
-                        print("prijato stopdt act, posilam stopdt con")
-                        self.active_session = False
-                    elif header == acpi.TESTFR_ACT:
-                        data = frame.pack(acpi.TESTFR_CON)
-                        comm_module.send_data(data)
-                        print("prijato testdt act, odeslano testdt. spojeni je aktivní")
-                    else:
-                        print("prislo neco divneho")
-                        
-                else:
-                    self.type_frame = ""
-            
-            
-            
-        
-            try:
-                packet = comm_module.receive_data()
-            except:
-                pass                      
-            header = packet[0]
-            data = packet[1]
-            if not (header[2] & 1):
-                print(f"I format {header[2] & 0xFF}")
-                self.type_frame = "I"
-            elif (header[2] & 3) == 1:
-                print(f"S format {header[2] & 0xFF}")
-                self.type_frame = "S"
-            elif (header[2] & 3) == 3: 
-                print(f"U format {header[2] & 0xFF}")
-                self.type_frame = "U"
-                header = header[2]
-                print(header)
-            else:
-                print("Nejaky jiny format")
-            
-            if self.type_frame == "I":
-                pass
-            elif self.type_frame == "S":
-                pass
-            elif self.type_frame == "U":
-                if header == acpi.STARTDT_ACT:
-                    data = frame.pack(acpi.STARTDT_CON)
-                    comm_module.send_data(data)
-                    print("prijato startdt act, posilam startdt con")
-                    self.active_session = True
-                elif header == acpi.STOPDT_ACT:
-                    data = frame.pack(acpi.STOPDT_CON)
-                    comm_module.send_data(data)
-                    print("prijato stopdt act, posilam stopdt con")
-                    self.active_session = False
-                elif header == acpi.TESTFR_ACT:
-                    data = frame.pack(acpi.TESTFR_CON)
-                    comm_module.send_data(data)
-                    print("prijato testdt act, odeslano testdt. spojeni je aktivní")
-                else:
-                    print("prislo neco divneho")
-                    
-            else:
-                self.type_frame = ""
+            message = client.recv(2048).decode('utf-8')
+            if message != '':
                 
-                print("ukoncen prenos, spojeni stale aktivní")
+                final_msg = username + '~' + message
+                self.send_messages_to_all(final_msg)
+
+            else:
+                print(f"The message send from client {username} is empty")
+
+   
+    # Function to send message to a single client
+    def send_message_to_client(self,client, message):
+
+        client.sendall(message.encode())
+
+    # Function to send any new message to all the clients that
+    # are currently connected to this server
+    def send_messages_to_all(self,message):
+        
+        for user in self.active_clients:
+
+            self.send_message_to_client(user[1], message)
+    
+    def active_transmission(self,client_socket, client_addr):
+        # kód pro indetifikaci přenosu uživatelských dat
+        try:
+            #client_socket.settimeout(acpi.T0)
+            
+            # Server will listen for client message that will contain the username
+            while True:
+                
+                header = self.comm_module.receive_length()
+                
+                #if username != '':
+                if header != '':
+                    #prompt_message = "SERVER~" + f"{username} added to the chat"
+                    #self.send_messages_to_all(prompt_message)
+                    
+                    self.type_frame = self.what_format(header)
+                    
+                    if self.type_frame == "I":
+                        # here is code that receive a rest of data
+                        pass
+                    
+                    elif self.type_frame == "S":
+                        # here is code that confirms correct reception of I format message
+                        pass
+                    
+                    elif self.type_frame == "U":                        
+                        if header == acpi.STARTDT_ACT:
+                            self.comm_module.send_data(self.frame.pack(acpi.STARTDT_CON))
+                            print("prijato startdt act, posilam startdt con")
+                            self.active_session = True
+                            
+                        elif header == acpi.STOPDT_ACT:
+                            self.comm_module.send_data(self.frame.pack(acpi.STOPDT_CON))
+                            print("prijato stopdt act, posilam stopdt con a přenos je ukončen")
+                            self.active_session = False
+                            break
+                        
+                        elif header == acpi.TESTFR_ACT:
+                            self.comm_module.send_data(self.frame.pack(acpi.TESTFR_CON))
+                            print("prijato testdt act, odeslano testdt. spojeni je aktivní")
+                            
+                        else:
+                            print("prislo neco divneho")
+                else:
+                    #print("Client username is empty")
+                    print("Receive packet is empty")
+            
+            ## procedura pro udržování aktivního spojení pomocí testdt mezi klientem a serverem
+            
         except socket.timeout:
-            print(f"Timeout - žádná data od klienta {client_address} během 30 sekund.")
-            # Zde můžete provést akce při timeoutu, například uzavření spojení a restart serveru
-            print("Konec spojení")
+            print(f"Timeout - žádná data od klienta {client_addr} během {acpi.T0} sekund.")
+            
+        except Exception as e:
+            print(f"Chyba při komunikaci s klientem {client_addr}: {e}")
+            
+
+    # Function to handle client
+    def client_handler(self,client_socket, client_addr):
+        
+        try:
+            
+            comm_module = CommModule(client_socket)
+            
+            # zkusit resit timeouty pomocí time.time()
+            cas = time.time()
+            #client_socket.settimeout(acpi.T0)
+            # Server will listen for client message that will contain the username
+            while True:
+                
+                #username = client.recv(2048).decode('utf-8')
+                # receive header for unpack header bytes
+                
+                data = comm_module.receive_traffic()
+                 
+                
+                
+                if data != '':
+                    #prompt_message = "SERVER~" + f"{username} added to the chat"
+                    #self.send_messages_to_all(prompt_message)
+                    
+                    if data[0] == 0:
+                        # I format - 
+                        frame_instance = data[1]
+                        pass
+                    elif data[0] == 1:
+                        # S format - logika potvrzování
+                        frame_instance = data[1]
+                        pass
+                    else:
+                        # U format - logika dat ASDU
+                        frame_instance = data[1]
+                        if frame_instance.get_type() == acpi.STOPDT_CON:
+                            break
+                    
+                
+                else:
+                    print("Receive packet is non-iec104")
+            
+            ## procedura pro udržování aktivního spojení pomocí testdt mezi klientem a serverem
+            
+        except socket.timeout:
+            print(f"Timeout - žádná data od klienta {client_addr} během {acpi.T0} sekund.")
+            
+        except Exception as e:
+            print(f"Chyba při komunikaci s klientem {client_addr}: {e}")
+            print("co teď ?")
+            
+        finally:
+            self.active_clients.remove((client_socket, client_addr))
             client_socket.close()
-        
+            for client in self.active_clients:
+                print(client)
+            print(f"Vlákno ukončeno. Spojení ukončení s {client_addr}")
 
-        except socket.error as e:
-            print(f"Chyba při komunikaci s klientem {client_address}: {e}")
+    # Main function
+    def start(self):
 
-        # finally:
-        #     # Uzavření soketu
-            
-        #     print("Konec spojení")
-        #     client_socket.close()
-            
-            
+        # Creating the socket class object
+        # AF_INET: we are going to use IPv4 addresses
+        # SOCK_STREAM: we are using TCP packets for communication
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-if __name__ == "__main__":
+        # Creating a try catch block
+        try:
+            # Provide the server with an address in the form of
+            # host IP and port
+            server_socket.bind((self.ip, self.port))
+            print(f"Running the server on {self.ip} {self.port}")
+            
+            # Set server limit
+            server_socket.listen(LISTENER_LIMIT)
+            
+        except:
+            print(f"Unable to bind to host {self.ip} and port {self.port}")
+
+
+        # This while loop will keep listening to client connections
+        while True:
+
+            print("Nasloucham dále...")
+            client, address = server_socket.accept()
+            print(f"Successfully connected to client {address[0]} {address[1]}")
+            self.active_clients.append((client, address))
+            
+            threading.Thread(target=self.client_handler, args=(client, address, )).start()
+
+
+if __name__ == '__main__':
     config_loader = ConfigLoader('config_parameters.json')
-    server = IEC104Server(config_loader.config['server']['ip_address'], config_loader.config['server']['port'])
+    server = ServerIEC104(config_loader.config['server']['ip_address'], config_loader.config['server']['port'])
     server.start()
-        
