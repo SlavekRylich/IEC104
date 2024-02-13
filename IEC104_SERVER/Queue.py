a95cf00582d00ac6587d6171441f7102b8c65c92
SlavekRylich <xrylic00@vutbr.cz>
2024-02-13 17:28:06 +0100

    create queue.py and session.py subclasses of CommModule

---
+++ IEC104_SERVER/Queue.py
@@ -0,0 +1,12 @@
+from CommModul import CommModule
+import struct
+
+class Queue(CommModule):
+    def __init__(self, socket):
+        super().__init__(socket)
+    
+    def __enter__():
+        pass    
+    
+    def __exit__(*exc_info):
+        pass
---
+++ IEC104_SERVER/Session.py
@@ -0,0 +1,13 @@
+from CommModul import CommModule
+
+
+class Session(CommModule):
+    
+    def __init__(self, socket):
+        super().__init__(socket)
+        
+    def __enter__():
+        pass
+        
+    def __exit__(*exc_info):
+        pass
