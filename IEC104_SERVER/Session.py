89bc3efcb2999cf8fb9706acd5901df3685e6ef5
SlavekRylich <xrylic00@vut.cz>
2024-02-15 22:08:04 +0100

    start with async logic

--- IEC104_SERVER/Session.py
+++ IEC104_SERVER/Session.py
@@ -25,13 +25,40 @@ class Session():
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         pass
     
-    def run_server(self):
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.ip,self.port))
+        logging.info(f"Connected to {self.ip}:{self.port}")
+        return self.socket
+
+
+    def start_server(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.ip,self.port))
         self.socket.listen(LISTENER_LIMIT)
         logging.info(f"Server listening on {self.ip}:{self.port}")
+
+    def disconnect(self):
+        logging.info(f"Disconnecting")
+        self.socket.close()
+
+    async def start_server_async(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.ip,self.port))
+        self.socket.listen(LISTENER_LIMIT)
+        logging.info(f"Server listening on {self.ip}:{self.port}")
+
+    async def connect_async(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        await self.socket.connect((self.ip,self.port))
+        logging.info(f"Connected to {self.ip}:{self.port}")
+        return self.socket
+
+    def disconnect(self):
+        logging.info(f"Server stopped")
+        self.socket.close()
     
         
     def __enter__():
